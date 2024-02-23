c244a6d81c0882d18b872ea9d5325677b34abe2e
acmexii <apexacme@gmail.com>
2024-02-23 23:33:10 +0000

    updated Code

--- store/src/test/java/fastfood/store/service/StoreServiceTest.java
+++ store/src/test/java/fastfood/store/service/StoreServiceTest.java
@@ -1,36 +1,75 @@
 package fastfood.store.service;
 
+import fastfood.store.domain.Store;
+import fastfood.store.events.OrderCancelled;
 import fastfood.store.events.OrderPlaced;
 import fastfood.store.repository.StoreRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.ContextConfiguration;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-class StoreServiceTest {
+@WebMvcTest(StoreService.class)
+@ContextConfiguration(classes = {StoreService.class})
+public class StoreServiceTest {
 
-    @Mock
+    @Autowired
+    @InjectMocks
+    private StoreService storeService;
+
+    @MockBean
     private StoreRepository storeRepository;
 
     @Mock
     private StreamBridge streamBridge;
 
-    @InjectMocks
-    private StoreService storeService;
-
     @Test
     void testWheneverOrderPlaced_AcceptOrder() {
-        OrderPlaced event = new OrderPlaced();
-        // Populate the event with test data
+        // Given
+        OrderPlaced orderPlaced = new OrderPlaced();
+        orderPlaced.setUserId("user1");
+        orderPlaced.setId(1L);
+        orderPlaced.setMenuId(10L);
+        orderPlaced.setQty(2);
+        orderPlaced.setAddress("123 Main St");
 
-        storeService.wheneverOrderPlaced_AcceptOrder().accept(event);
+        Message<OrderPlaced> message = MessageBuilder.withPayload(orderPlaced).build();
 
-        // Verify the interactions or state changes expected
+        // When
+        storeService.wheneverOrderPlaced_AcceptOrder().accept(message);
+
+        // Then
+        verify(storeRepository).save(any(Store.class));
     }
 
-    // Additional tests for other events and scenarios
+    @Test
+    void testWheneverOrderCancelled_CancelCook() {
+        // Given
+        OrderCancelled orderCancelled = new OrderCancelled();
+        orderCancelled.setId(1L);
+
+        Store store = new Store();
+        store.setOrderId(1L);
+        store.setStatus("OrderAccepted");
+
+        when(storeRepository.findByOrderId(1L)).thenReturn(store);
+
+        Message<OrderCancelled> message = MessageBuilder.withPayload(orderCancelled).build();
+
+        // When
+        storeService.wheneverOrderCancelled_CancelCook().accept(message);
+
+        // Then
+        verify(storeRepository).save(any(Store.class));
+    }
 }
